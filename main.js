886e06750bde1f278b666c4d37c6b02ca1e94078
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-22 10:47:22 +0900

    마우스 호버시 이펙트 추가 및 반응형으로 수정

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-/* 
-promise 객체
-객페에 담겨있는 데이터ㅡ이 상태값을 가지고 있는
-pending : 대기
-fullfiled : 성공적으로 작업의 수행이 완료된 상태
-rejected : 작업의 요청이 거절된 상태
-
-작업시간이 오래걸리는 업무를 수행할때 promise로 반환하면 자동적으로 해당 작업 완료 이후 동기적으로 다음 작업을 선형화해서 수행 (es6)
-*/
-
 const section = document.querySelector('section');
 
 fetch('DB/department.json')
@@ -18,6 +8,7 @@ fetch('DB/department.json')
 			tags += `
       <article>
       <div class='pic'>
+        <img src='img/${data.pic}'></img>
         <img src='img/${data.pic}'></img>
       </div>
       <h2>${data.position}</h2>
