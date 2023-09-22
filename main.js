0e58a996b76384e7858277bdf3703ad0706abe09
jinkyo-09 <rlawlsry0911@gmail.com>
2023-09-22 09:36:19 +0900

    fetch 효율적으로 수정

--- main.js
+++ main.js
@@ -5,23 +5,18 @@ pending : 대기
 fullfiled : 성공적으로 작업의 수행이 완료된 상태
 rejected : 작업의 요청이 거절된 상태
 
-작업시간이 오래걸리는 업무를 수행할때 promise로 반환하면 자동적으로 해당 작업 완료 이후 동기적으로 다음 작업을 선형화해서 수행 (es5)
+작업시간이 오래걸리는 업무를 수행할때 promise로 반환하면 자동적으로 해당 작업 완료 이후 동기적으로 다음 작업을 선형화해서 수행 (es6)
 */
 
 fetch('DB/department.json')
-	//fetch가 반환한 프로미스를 동기적으로 호출
 	.then((data) => {
-		//이전 메서드에서 반환된 promise 객체가 fullfiled 상태일때 실행
-		//json형태로 parsing되지 않은 raq data
-		const result = data.json();
-		//json 형태로 반환시 promise로 반환
-		result.then((json) => {
-			//json으로 반환된 데이터를 then으로 확인
-			console.log(json);
-		});
+		//첫번째 then구문에서 받아진 promise를 다시 바로 리턴
+		//두번째 구문에서 해당 데이터를 동기적으로 활용 가능
+		return data.json();
+	})
+	.then((json) => {
+		console.log(json);
 	})
 	.catch((err) => {
-		//이전 메서드에서 반환된 promise 객체가 rejected 상태일때 실행
-		//오류가 발생했을때 runtime에러(실시간에러)가 발생하지 않도록 예외사항을 준비
 		console.log(err);
 	});
